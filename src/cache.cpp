2c3e157c42f07d9b7c834117e370987c401b680a
Frederik Gladhorn <frederik.gladhorn@nokia.com>
2011-02-16 15:05:30 +0100

    Only emit AddAccessible once.

--- src/cache.cpp
+++ src/cache.cpp
@@ -51,11 +51,6 @@ QSpiAccessibleCache::QSpiAccessibleCache(QObject *root, QDBusConnection c)
 
     adaptor = new CacheAdaptor(this);
     c.registerObject(QSPI_OBJECT_PATH_CACHE, this, QDBusConnection::ExportAdaptors);
-
-    connect (this, SIGNAL(AddAccessible(QSpiAccessibleCacheItem)),
-             adaptor, SIGNAL(AddAccessible(QSpiAccessibleCacheItem)));
-    connect (this, SIGNAL(RemoveAccessible(QSpiObjectReference)),
-             adaptor, SIGNAL(RemoveAccessible(QSpiObjectReference)));
 }
 
 /*---------------------------------------------------------------------------*/
