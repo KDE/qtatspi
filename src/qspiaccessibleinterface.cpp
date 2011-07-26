39919c1274ed877ecf35cfe5688e13c76f5832a8
Frederik Gladhorn <frederik.gladhorn@nokia.com>
2011-07-26 11:45:26 +0200

    Cleanup, license.

--- src/qspiaccessibleinterface.cpp
+++ src/qspiaccessibleinterface.cpp
@@ -1,3 +1,22 @@
+/*
+ * D-Bus AT-SPI, Qt Adaptor
+ *
+ * Copyright 2011 Nokia Corporation and/or its subsidiary(-ies).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "qspiaccessibleinterface.h"
 
 #include <qdebug.h>
@@ -19,27 +38,21 @@ bool QSpiAccessibleInterface::handleMessage(QAccessibleInterface *interface, int
         v.setValue((uint) qSpiRoleMapping[interface->role(child)].spiRole());
         QDBusMessage reply = message.createReply(v);
         connection.send(reply);
-
         return true;
     } else if (function == "GetName") {
         sendReply(connection, message, interface->text(QAccessible::Name, child));
         return true;
     } else if (function == "GetRoleName") {
-
         QVariant v;
         v.setValue(qSpiRoleMapping[interface->role(child)].name());
         QDBusMessage reply = message.createReply(v);
         connection.send(reply);
-
         return true;
     } else if (function == "GetLocalizedRoleName") {
-
         QVariant v;
         v.setValue(qSpiRoleMapping[interface->role(child)].localizedName());
         QDBusMessage reply = message.createReply(v);
         connection.send(reply);
-
-
         return true;
     } else if (function == "GetChildCount") {
         int childCount = child ? 0 : interface->childCount();
@@ -74,7 +87,6 @@ bool QSpiAccessibleInterface::handleMessage(QAccessibleInterface *interface, int
         ref.setValue(v);
         sendReply(connection, message, ref);
         return true;
-
     } else if (function == "GetChildAtIndex") {
         int index = message.arguments().first().toInt();
         QString path;
